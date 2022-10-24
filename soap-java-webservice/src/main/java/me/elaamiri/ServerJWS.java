4b67178cffef231bd6c6676af500a3ad1a5cd57e
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-24 10:37:09 +0100

    server done + wsdl

--- soap-java-webservice/src/main/java/me/elaamiri/ServerJWS.java
+++ soap-java-webservice/src/main/java/me/elaamiri/ServerJWS.java
@@ -1,7 +1,16 @@
 package me.elaamiri;
 
+import jakarta.xml.ws.Endpoint;
+import me.elaamiri.webservice.BankWebservice;
+
 public class ServerJWS {
+    // The webservice is accessible only in local
+    private static String SERVER_ADDRESS_LOCAL = "http://localhost:9292/";
+    // The webservice accessible from any address (From Other address )
+    private static String SERVER_ADDRESS = "http://0.0.0.0:9292/";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Endpoint.publish(SERVER_ADDRESS, new BankWebservice());
+        System.out.println(String.format("Web service deployed on [%s]",SERVER_ADDRESS));
+
     }
 }
