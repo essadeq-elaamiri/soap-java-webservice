3c6fd85d3ec64f7cc710da6f86cd6d071208775d
essadeq-elaamiri <essadeq0701@gmail.com>
2022-10-24 10:13:18 +0100

    in progress 1

---
+++ soap-java-webservice/src/main/java/me/elaamiri/webservice/Account.java
@@ -0,0 +1,6 @@
+package me.elaamiri.webservice;
+
+public class Account {
+    private  int code;
+    private double solde
+}
---
+++ soap-java-webservice/src/main/java/me/elaamiri/webservice/BackWebservice.java
@@ -0,0 +1,9 @@
+package me.elaamiri.webservice;
+
+public class BackWebservice {
+    public double conversion(double amount){
+        return amount * 10.42;
+    }
+
+    public Com
+}
